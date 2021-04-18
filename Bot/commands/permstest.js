b344f01a539233fd5ab4df54dae804ecc297c601
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-18 16:34:42 -0400

    smart

---
+++ Bot/commands/permstest.js
@@ -0,0 +1,7 @@
+module.exports={
+    name:"permstest",
+    permissions:"MANAGE_ROLES",
+    execute(message,args){
+        message.reply("This user has the MANAGE_ROLES permission")
+    },
+};
---
+++ Bot/commands/smart.js
@@ -0,0 +1,9 @@
+module.exports={
+    name:"smart",
+    description:"Totally Accurate IQ test",
+    aliases:["smart","iq"],
+    cooldown:5,
+    execute(message,args){
+        message.reply("has an IQ of"+Math.floor(Math.random()*201))
+    }
+}
