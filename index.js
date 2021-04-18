a0be233e60e1cf70b59cb9b4e857583f3983824e
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-18 12:55:59 -0400

    setup

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+Made by Aiden (SomeAspy)
+GitHub: https://github.com/SomeAspy/SimpleBot
+License: GPLv3
+*/
+
+//Required Packages
+require("dotenv").config()
+const Discord=require("discord.js")
