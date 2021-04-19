12306f8fe08fd90115d8baeec6a0aa290a4d2bc6
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-19 19:43:05 -0400

    Avatar fixes

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
 module.exports={
     randColor:function(){return Math.floor(Math.random()*16777215).toString(16);},
-    mentionToUser:function(mention){
+    mentionToUser:function(client,mention){
         let matches= mention.match(/^<@!?(\d+)>$/);
         if(!matches)return;
         return client.users.cache.get(matches[1]);
