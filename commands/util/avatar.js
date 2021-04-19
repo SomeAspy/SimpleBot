5c9a56a02d4bc5946b06bf0c1cd288a4ac6a7b69
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-19 18:39:58 -0400

    kinda dumb

--- commands/util/avatar.js
+++ commands/util/avatar.js
@@ -8,12 +8,13 @@ module.exports={
     cooldown:5,
     execute(message,args){
         let user;
-        //if(!message.mentions.users.size){
+        if(!message.mentions.users.size){
             user=message.author.displayAvatarURL({dynamic:true})
-        //}
+        }
         const embed=new Discord.MessageEmbed()
         .setColor(lib.RandColor)
         .setImage(user)
 
+    message.channel.send(embed)
     }
 }
