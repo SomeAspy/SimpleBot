b512fe237c6bda225dc39752335f129b47fb3ce7
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-19 13:16:59 -0400

    fix reload again

--- commands/util/reload.js
+++ commands/util/reload.js
@@ -7,7 +7,7 @@ module.exports={
         const commandName=args[0].toLowerCase();
         const command=message.client.commands.get(commandName)||message.client.commands.find(cmd=>cmd.aliases&&cmd.aliases.includes(commandName));
         if(!command)return message.channel.send('There is no command matching that name to reload');
-        const commandFolders=fs.readdirSync('./commands');
+        const commandFolders=fs.readdirSync('../');
         const folderName=commandFolders.find(folder=>fs.readdirSync(`./commands/${folder}`).includes(`${command.name}.js`));
         delete require.cache[require.resolve(`../${folderName}/${command.name}.js`)];
         try{
