3f924249ecd78c128758bfab9b5d41a93a7480c8
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-19 19:56:27 -0400

    source

---
+++ commands/info/source.js
@@ -0,0 +1,6 @@
+module.exports={
+    name:'source',
+    description:'Where you can get the source code',
+    aliases:['github','code','source','sourcecode','os','contribute'],
+    execute(message){message.reply('You can find my source code here!\nhttps://github.com/SomeAspy/SimpleBot')}
+}
