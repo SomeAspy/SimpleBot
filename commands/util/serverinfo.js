bf40b91664e096a49a997f8c01a218b51732c3fa
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-19 17:23:52 -0400

    serverInfo update

--- commands/util/serverinfo.js
+++ commands/util/serverinfo.js
@@ -7,6 +7,14 @@ module.exports={
     cooldown:5,
     aliases:["server"],
     execute(message,args){
+        let vLevel;
+        switch(message.guild.verificationLevel){
+            case 'NONE':vLevel='Anyone can talk.';break;
+            case 'LOW':vLevel='Verified email required to talk.';break;
+            case 'MEDIUM':vLevel='Must be on discord for 5 minutes to talk.';break;
+            case 'HIGH':vLevel='Must be in this server for 10 minutes to talk';break;
+            case 'VERY_HIGH':vLevel='Must have a verified phone number to talk';break;
+        }
         const embed=new Discord.MessageEmbed()
         .setColor(randColor())
         .setTitle(`${message.guild.name}'s stats`)
@@ -14,7 +22,7 @@ module.exports={
         .addFields(
             {name:"Server Owner:",value:message.guild.owner},
             {name:"Creation Date:",value:message.guild.createdAt},
-            {name:"Verification Level:",value:message.guild.verificationLevel},
+            {name:"Verification Level:",value:vLevel},
             {name:"Content Filter:",value:message.guild.explicitContentFilter},
             {name:"Boosts:",value:message.guild.premiumSubscriptionCount},
             {name:"members:",value:message.guild.memberCount}
