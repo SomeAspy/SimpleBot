7e55bbe319224fb5a8da2755332899bd99a7874a
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-18 20:29:17 -0400

    why

--- Bot/commands/util/help.js
+++ Bot/commands/util/help.js
@@ -24,6 +24,13 @@ module.exports={
         }
         const commandName=args[0].toLowerCase();
         const command=message.client.commands.get(commandName)||message.client.commands.find(cmd=>cmd.aliases&&cmd.aliases.includes(commandName));
-        if(!command)return message.channel.send('The command you requested help for does not exist!')
+        if(!command){return message.channel.send('The command you requested help for does not exist!');
+        }
+        data.push(`**Name:** ${command.name}`);
+        if(command.aliases)data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+        if(command.description)data.push(`**Description:** ${command.description}`);
+        if(command.usage)data.push(`**Usage: ${prefix}${command.name} ${command.usage}`);
+        data.push(`**Cooldown:** ${command.cooldown||3} second(s)`);
+        message.channel.send(data,{split:true});
     }
 }
