001ee3ad90f12172bb09232acea37701121c4e5b
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-19 20:35:32 -0400

    copyright

---
+++ commands/info/license.js
@@ -0,0 +1,7 @@
+module.exports={
+    name:'license',
+    description:'legal stuff',
+    cooldown:60,
+    aliases:['copyright'],
+    execute(message){message.reply('I am licensed under the GPLv3 license.\nhttps://www.gnu.org/licenses/gpl-3.0.txt')}
+}
