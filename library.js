2f56d183c9a606d7803bd7bfa584dbcb9ec3353e
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-20 13:05:24 -0400

    yeah it didn't work

--- library.js
+++ library.js
@@ -1,15 +1,15 @@
 const client = require("./init");
-function guild(guildID){return client.guilds.cache.get(guildID)}
+//function guild(guildID){return client.guilds.cache.get(guildID)}
 module.exports={
     randColor:function(){return Math.floor(Math.random()*16777215).toString(16);},
     mentionToUser:function(mention,memberMode=false,message){
         const matches=mention.match(/^<@!?(\d+)>$/);
         if(!matches)return;
-        const id=matches[1];
+        const mid=matches[1];
         if(!memberMode){
-        return client.users.cache.get(id);
+        return client.users.cache.get(mid);
         }else{
-            return guild(message.guild.id).member(id)
+            return client.guilds.cache.get(message.guild.id).member(mid)
         }
     }
 }
