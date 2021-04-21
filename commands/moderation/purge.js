9e14b08e6a5224ec9c83a9882da50d22ed03dac1
SomeAspy <33640860+SomeAspy@users.noreply.github.com>
2021-04-21 18:28:03 -0400

    changes

---
+++ commands/moderation/purge.js
@@ -0,0 +1,20 @@
+import { MessageEmbed } from 'discord.js';
+import { randColor } from '../../library.js';
+export const name = "purge";
+export const description = "Purge x number of messages, (age is limited to 2 weeks duw to api)";
+export const args = true;
+export const usage = "<number of messages to delete>";
+export const permissions = "MANAGE_MESSAGES";
+export const guildOnly = true;
+export const aliases=['prune']
+export function execute(message, args, client) {
+    let count=args[0]
+    if(isNaN(count)){return message.reply('enter a valid number!')}
+    message.channel.bulkDelete(count).then(messages=>count=messages.size)
+    let embed = new MessageEmbed()
+        .setTitle(`Deleted ${count} messages!`)
+        .setColor(randColor())
+        .setTimestamp();
+
+    message.channel.send(embed);
+}
