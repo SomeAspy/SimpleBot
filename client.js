10ff04cae58ce240fee1acf84e42ec57a5ab10ad
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-04-27 11:13:31 -0400

    fix merge mistakes

--- client.js
+++
@@ -1,6 +0,0 @@
-import {Client} from 'discord.js';
-import dotenv from 'dotenv'
-dotenv.config()
-export const client=new Client()
-client.login(process.env.DISCORD_TOKEN)
-client.once('ready',()=>console.log('Bot is ready to accept commands!'))
