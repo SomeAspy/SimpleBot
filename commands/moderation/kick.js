1ca58f1d4416c058fba7b9c7fe07cf63dc38ffc0
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-05-02 21:25:27 -0400

    kick command finally works

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,25 +1,31 @@
-import{secondArg,mentionToUser}from'../../library.js'
-import { MessageEmbed } from 'discord.js';
+import{mentionToUser}from'../../library.js'
+import{MessageEmbed}from'discord.js';
 export const name = "kick";
 export const description = "Kick a member";
 export const args = true;
 export const usage = "<user>";
 export const permissions = "KICK_MEMBERS";
 export const guildOnly = true;
-export function execute(message, args) {
-    let kickReason='(No reason given)'
-    console.log(args)
+export async function execute(message, args) {
+    let kickReason;
+    let OK=true
     let user = mentionToUser(args.shift());
-    console.log(args)
-    if(!args.length===0){
-        kickReason=secondArg(args)
+    kickReason=args.pop()
+    if(kickReason==undefined){
+        kickReason='(No reason given)'
     }
     if (!user) { return message.reply('Invalid User!'); }
-    message.mentions.members.first().kick({reason:kickReason});
-    let embed = new MessageEmbed()
-        .setTitle(`Kicked ${user.username} with reason ${kickReason}.`)
-        .setColor('#ff0000')
-        .setTimestamp();
-
-    message.channel.send(embed);
+        await message.mentions.members.first().kick({reason:kickReason}).catch(error=>{
+        if(error.code===50013){
+            message.reply('I cannot perform that action on that user! (No Perms)')
+            OK=false
+        }
+    })
+    if(OK){
+        let embed = new MessageEmbed()
+            .setTitle(`Kicked ${user.username} with reason ${kickReason}.`)
+            .setColor('#ff0000')
+            .setTimestamp();
+        message.channel.send(embed);
+    }
 }
